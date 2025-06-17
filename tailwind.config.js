0bb5b30d842989267fbc7d37eb1ab3fca9c301a6
Rémi <wattier.remi@gmail.com>
2025-06-17 10:23:34 +0200

    Initialize app component

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{html,ts}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+// This is a Tailwind CSS configuration file that specifies the content files to scan for class names and extends the default theme.
