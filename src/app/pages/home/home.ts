c3107c07aea81a68e1526fa161b8f8b0485edffd
Rémi <wattier.remi@gmail.com>
2025-06-17 16:35:37 +0200

    Programme + footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { Tabs } from './tabs/tabs';
+import { Programme } from './programme/programme';
+import { Footer } from '../../footer/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Tabs],
+  imports: [Hero, Tabs, Programme, Footer],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
---
+++ src/app/pages/home/programme/programme.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programme',
+  imports: [],
+  templateUrl: './programme.html',
+  styleUrl: './programme.scss'
+})
+export class Programme {
+
+}
