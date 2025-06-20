1ea58d307a46350b7d6de6a3c7fb0f39862dc8fb
Rémi <wattier.remi@gmail.com>
2025-06-21 00:11:04 +0200

    contenus des tabs + affichage

--- src/app/components/home/tabs/tabs.ts
+++ src/app/components/home/tabs/tabs.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Programme } from '../programme/programme';
+import { BonASavoir } from '../bon-a-savoir/bon-a-savoir';
+import { Inclus } from '../inclus/inclus';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
+  imports: [CommonModule, Programme, BonASavoir, Inclus],
   templateUrl: './tabs.html',
   styleUrl: './tabs.scss'
 })
 export class Tabs {
+  activeTab = signal<'programme' | 'bonasavoir' | 'inclus'>('programme');
+
+  setTab(tab: 'programme' | 'bonasavoir' | 'inclus') {
+    this.activeTab.set(tab);
+  }
 
 }
