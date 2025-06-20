ac4f9a22aaa7036f1d9f7c1f9004e83af30c795d
Rémi <wattier.remi@gmail.com>
2025-06-20 17:41:33 +0200

    fignolage main page voyage

--- src/app/pages/home/programme/programme.ts
+++ src/app/pages/home/programme/programme.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-programme',
   imports: [],
+  standalone: true,
   templateUrl: './programme.html',
+  animations: [
+    trigger('expandCollapse', [
+      state('open', style({ height: '*', opacity: 1 })),
+      state('closed', style({ height: '0px', opacity: 0 })),
+      transition('closed <=> open', [animate('1s ease-in-out')])
+    ])
+  ],
   styleUrl: './programme.scss'
 })
 export class Programme {
 
+  showFullText = false;
+  showAllDetails = false;
+
+
+  expandedHeight = 0;
+  @ViewChild('expandingBlock', { static: false }) expandingBlockRef!: any;
+
+
+  toggleFullText() {
+    this.showFullText = !this.showFullText;
+  }
+
+  toggleAllDetails() {
+    this.showAllDetails = !this.showAllDetails;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  provideAnimations()
+  ]
 });
