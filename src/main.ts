7c16eda57838faffce69ad47dcfba6bd6856c803
Rémi <wattier.remi@gmail.com>
2025-07-08 21:18:57 +0200

    Scroll to top

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
-import { provideRouter } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { provideRouter, Router, NavigationEnd } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(App, {
   providers: [provideRouter(routes),
   provideAnimations()
   ]
+}).then(appRef => {
+  const injector = appRef.injector;
+  const router = injector.get(Router);
+  const viewportScroller = injector.get(ViewportScroller);
+
+  router.events.pipe(
+    filter((e): e is NavigationEnd => e instanceof NavigationEnd)
+  ).subscribe(() => {
+    viewportScroller.scrollToPosition([0, 0]);
+  });
 });
