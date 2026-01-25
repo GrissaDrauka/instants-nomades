33601d671b1f9082ff1cfad0847d288ac6994e81
Rémi <wattier.remi@gmail.com>
2026-01-25 18:57:43 +0100

    Optimisations conversion + nettoyage redondances page Le Trek
    - Page Le Trek : suppression section Activités (→ CTA vers page dédiée)
    - Page Le Trek : suppression section Inclus/Non inclus (existe sur Accueil + Tarifs)
    - Page Le Trek : remplacement date obsolète par CTA Prochains Départs
    - Page Tarifs : refonte cards Riads en 2x2, titres plus grands
    - Page Tarifs : emails en vert, téléphones en noir
    - Footer : ajout lien Prochains Départs
    - Accueil : ajout encart CTA vers Prochains Départs 2026/2027

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Hero } from './hero/hero';
 import { Tabs } from './tabs/tabs';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Tabs],
+  imports: [Hero, Tabs, RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
